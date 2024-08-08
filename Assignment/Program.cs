8953b14f986b6793cc04117a14c15b43deebe9a4
MohamedAdel510 <119543542+MohamedAdel510@users.noreply.github.com>
2024-08-08 04:15:56 +0300

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,264 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using static Assignment.ListGenerator;
+using System.IO;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.Intrinsics.X86;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Aggregate Operators
+
+            #region Q1 Get the total units in stock for each product category.
+
+            //var Result = ProductsList.Where(P => P.UnitsInStock > 0).GroupBy(P => P.Category).Select(C => new {Name = C.Key, Units = C.Count() });
+
+            #endregion
+
+            #region Q2 Get the cheapest price among each category's products
+
+            //var Result = ProductsList.GroupBy(P => P.Category).Select(C => C.Min(P => P.UnitPrice));
+
+            #endregion
+
+            #region Q3 Get the products with the cheapest price in each category (Use Let) ???
+
+            //var Result = from P in ProductsList
+            //             group P by P.Category into C
+            //             let 
+
+
+            //var Result = ProductsList.GroupBy(P => P.Category).Select(C => C.Min());
+
+            //ProductID: 24,ProductName: Guaraná Fantástica, CategoryBeverages, UnitPrice:4.5000,UnitsInStock: 20
+            //ProductID: 3,ProductName: Aniseed Syrup, CategoryCondiments, UnitPrice:10.0000,UnitsInStock: 13
+            //ProductID: 74,ProductName: Longlife Tofu, CategoryProduce, UnitPrice:10.0000,UnitsInStock: 4
+            //ProductID: 54,ProductName: Tourtière,CategoryMeat / Poultry,UnitPrice: 7.4500,UnitsInStock: 21
+            //ProductID: 13,ProductName: Konbu,CategorySeafood,UnitPrice: 6.0000,UnitsInStock: 24
+            //ProductID: 33,ProductName: Geitost,CategoryDairy Products, UnitPrice:2.5000,UnitsInStock: 112
+            //ProductID: 19,ProductName: Teatime Chocolate Biscuits,CategoryConfections,UnitPrice: 9.2000,UnitsInStock: 25
+            //ProductID: 52,ProductName: Filo Mix, CategoryGrains/ Cereals,UnitPrice: 7.0000,UnitsInStock: 38
+
+            #endregion
+
+            #region Q4 Get the most expensive price among each category's products
+
+            //var Result = ProductsList.GroupBy(P => P.Category).Select(C => C.Max(P => P.UnitPrice));
+
+            #endregion
+
+            #region Q5 Get the products with the most expensive price in each category.
+
+            //var Result = ProductsList.GroupBy(P => P.Category).Select(C => C.Max());
+
+            #endregion
+
+            #region Q6 Get the average price of each category's products.
+
+            //var Result = ProductsList.GroupBy(P => P.Category).Select(C => C.Average(P => P.UnitPrice));
+
+            #endregion
+
+            #endregion
+
+            #region Set Operators
+
+            #region Q1 Find the unique Category names from Product List
+
+            //var Result = ProductsList.Select(P => P.Category).Distinct();
+
+            #endregion
+
+            #region Q2 Produce a Sequence containing the unique first letter from both product and customer names.
+
+
+            //var Result = ProductsList.Select(P => P.ProductName[0]).Union(CustomersList.Select(C => C.CustomerName[0]));
+
+            #endregion
+
+            #region Q3 Create one sequence that contains the common first letter from both product and customer names.
+
+            //var Result = ProductsList.Select(P => P.ProductName[0]).Intersect(CustomersList.Select(C => C.CustomerName[0]));
+
+            #endregion
+
+            #region Q4 Create one sequence that contains the first letters of product names that are not also first letters of customer names.
+
+            //var Result = ProductsList.Select(P => P.ProductName[0]).Except(CustomersList.Select(C => C.CustomerName[0]));
+
+            #endregion
+
+            #region Q5 Create one sequence that contains the last Three Characters in each name of all customers and products, including any duplicates
+
+            //var ProductNameChars = ProductsList.Select(P => P.ProductName.Substring(P.ProductName.Length - 3));
+            //var CustomerNameChars = CustomersList.Select(C => C.CustomerName.Substring(C.CustomerName.Length - 3));
+
+            //var Result = ProductNameChars.Concat(CustomerNameChars);
+
+            #endregion
+
+            #endregion
+
+            #region Partitioning Operators
+
+            #region Q1 Get the first 3 orders from customers in Washington
+
+            //var Result = CustomersList.Where(C => C.City == "Washington").SelectMany(C => C.Orders);
+
+            #endregion
+
+            #region Q2 Get all but the first 2 orders from customers in Washington.????
+
+            //var Result = CustomersList.Where(C => C.City == "Washington").SelectMany(C => C.Orders).Take(2).Concat();
+
+            #endregion
+
+            #region Q3 Return elements starting from the beginning of the array until a number is hit that is less than its position in the array.
+
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            //var Result = numbers.TakeWhile((N, I) => N > I);
+
+            #endregion
+
+            #region Q4 Get the elements of the array starting from the first element divisible by 3.
+
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = numbers.SkipWhile(N => N % 3 != 0);
+
+            #endregion
+
+            #region Q5 Get the elements of the array starting from the first element less than its position.
+            ////              [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            //var Result = numbers.SkipWhile((N, I) => N > I);
+
+            #endregion
+
+            #endregion
+
+            #region Quantifiers Operators
+
+            #region Q1 Determine if any of the words in dictionary_english.txt (Read dictionary_english.txt into Array of String First) contain the substring 'ei'
+
+            //List<string> DictionaryEnglish = new List<string>();
+            //StreamReader reader;
+            //try
+            //{
+            //    reader = new StreamReader("dictionary_english.txt");
+            //    DictionaryEnglish.Add(reader.ReadLine());
+
+            //    while (reader.ReadLine() != null)
+            //    {
+            //        DictionaryEnglish.Add(reader.ReadLine());
+            //        //Console.WriteLine(reader.ReadLine());
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //var Result = DictionaryEnglish.Any(W => W.Contains("ei"));
+            #endregion
+
+            #region Q2  Return a grouped a list of products only for categories that have at least one product that is out of stock.
+
+            //var Result = ProductsList.GroupBy(P => P.Category).Where(C => C.Any(P => P.UnitsInStock == 0));
+
+            #endregion
+
+            #region Q3 Return a grouped a list of products only for categories that have all of their products in stock.
+
+            //var Result = ProductsList.GroupBy(P => P.Category).Where(C => C.All(P => P.UnitsInStock > 0));
+
+            #endregion
+
+            #endregion
+
+            #region Grouping Operators
+
+            #region Q1 Use group by to partition a list of numbers by their remainder when divided by 5
+
+            //List<int> numbers = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            //var Result = numbers.GroupBy(N => N % 5);
+
+            //foreach (var remainder in Result)
+            //{
+            //    Console.WriteLine($"Numbers with a remainder {remainder.Key} when divided by 5:");
+            //    foreach (var N in remainder)
+            //    {
+            //        Console.WriteLine(N);
+            //    }
+            //}
+
+            #endregion
+
+            #region Q2 Uses group by to partition a list of words by their first letter Use dictionary_english.txt for Input
+
+            //List<string> DictionaryEnglish = new List<string>();
+            //StreamReader reader;
+            //try
+            //{
+            //    reader = new StreamReader("dictionary_english.txt");
+            //    DictionaryEnglish.Add(reader.ReadLine());
+
+            //    while (reader.ReadLine() != null)
+            //    {
+            //        DictionaryEnglish.Add(reader.ReadLine());
+            //        //Console.WriteLine(reader.ReadLine());
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //var Result = DictionaryEnglish.GroupBy(W => W[0]);
+
+            //foreach (var Letter in Result)
+            //{
+            //    Console.WriteLine($"======{Letter.Key}======");
+            //    foreach (var W in Letter)
+            //    {
+            //        Console.WriteLine(W);
+            //    }
+            //}
+
+            #endregion
+
+            #region Q3 Use Group By with a custom comparer that matches words that are consists of the same Characters Together
+
+            //string[] Arr = { "from", "salt", "earn", " last", "near", "form" };
+
+            
+
+            #endregion
+
+            #endregion
+
+            //Console.WriteLine(Result);
+
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //foreach (var category in Result)
+            //{
+            //    Console.WriteLine(category.Key);
+            //    foreach (var P in category)
+            //    {
+            //        Console.WriteLine($"  {P.ProductName}");
+            //    }
+            //}
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using static Demo.ListGenerator;
+//using System.Text.RegularExpressions;
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  Aggregate Operators [ Count , Sum , Min , Max , Average ]
+
+            //var Result = ProductsList.Count();
+
+            //var Result = ProductsList.Min();
+            //var Result = ProductsList.Max(); 
+
+
+            //Overload
+            //var Result = ProductsList.Max(P => P.ProductName.Length);
+            //Result = ProductsList.Min(P => P.ProductName.Length);
+
+            //Hybrid syntax 
+            // Get the product that have the minimum name length
+
+            //var MinLength = ProductsList.Min(P => P.ProductName.Length);
+            //var Result = (from P in ProductsList
+            //              where P.ProductName.Length == MinLength
+            //              select P).FirstOrDefault();
+            //Console.WriteLine(Result?.ProductName ?? "Mohamed");
+
+            //var Result = ProductsList.Sum(P => P.UnitPrice);
+            //Result = ProductsList.Average(P => P.UnitPrice);
+
+            // Aggregate
+
+            //string[] Names = { "Moahmed", "Adel", "Sara", "Adel", "Nourhan", "Tareq" };
+            //var Result = Names.Aggregate((S1, S2) => $"{S1} {S2}");
+
+            #endregion
+
+            #region Casting [Conversion] Operators - Immediate Execution
+            // 1- ToList
+            //List<Product> Result = ProductsList.Where(P => P.UnitsInStock == 0).ToList();
+
+            //2- To Array
+
+            //Product[] Result = ProductsList.Where(P => P.UnitsInStock == 0).ToArray();
+
+            //3- To Dictionary
+            //Dictionary<long, Product> Result = ProductsList.Where(P => P.UnitsInStock == 0)
+            //    .ToDictionary(P => P.ProductID);
+
+            //Dictionary<long, string> Result = ProductsList.Where(P => P.UnitsInStock == 0)
+            //    .ToDictionary(P => P.ProductID, P => P.ProductName);
+
+            //4- To HashSet
+
+            //HashSet<Product> Result = ProductsList.Where(P => P.UnitsInStock == 0).ToHashSet();
+
+            //5- OfType
+            //ArrayList Obj = new ArrayList() 
+            //{
+            //    "Omer",
+            //    "Mohamed",
+            //    "Nouwara",
+            //    1,2,3
+            //};
+            //var Result = Obj.OfType<string>();
+            #endregion
+
+            #region Generation Operators - Deferred Execution [Range - Repeat - Empty]
+
+            //var Result = Enumerable.Range(0, 100);
+            //Result = Enumerable.Repeat(2, 100);
+            //var Result = Enumerable.Empty<Product>();
+
+            #endregion
+
+            #region Set Operators [Union Family] - Deferred Execution
+
+            var Seq01 = Enumerable.Range(0, 100);  // 0 -> 99
+            var Seq02 = Enumerable.Range(50, 100); // 50 -> 149
+
+            //var Result = Seq01.Union(Seq02); // 0 - 149 Without Dublication
+
+            //Result = Seq01.Concat(Seq02); // 0 -> 99 + 50 -> 149
+
+            //Result = Seq01.Intersect(Seq02); // 50 -> 99
+
+            //Result = Seq01.Except(Seq02); // 0 -> 49
+
+            //Result = Seq01.Concat(Seq02).Distinct(); // as Union 0 - 149 Without Dublication
+
+            #endregion
+
+            #region Quantifier Operator - Return boolean [Any - All - SequanceEqual]
+            //var Result = ProductsList.Any();
+            //var Result = ProductsList.Any(P => P.UnitsInStock == 0);
+
+            //var Result = ProductsList.All(P => P.UnitsInStock == 0);
+
+            //var Result = Seq01.SequenceEqual(Seq02);
+            #endregion
+
+            #region Zipping Operator [Zip]
+
+            int[] Numbers  = Enumerable.Range(0, 10).ToArray();
+            string[] Names = { "Omer", "Ahmed", "Ali" };
+
+
+            //var Result = Numbers.Zip(Names);
+
+            //var Result = Numbers.Zip(Names, (Number, Name) => new { ind = Number, Name = Name });
+            #endregion
+
+            #region Grouping Operators
+
+            #region Get Products Grouped by Category
+
+            //Fluent
+            //var Result = ProductsList.GroupBy(P => P.Category);
+
+            //Query
+            //var Result = from P in ProductsList
+            //             group P by P.Category;
+
+            #endregion
+
+            #region Get Products in Stock Grouped by Category
+
+            // fluent
+            //var Result = ProductsList.Where(P => P.UnitsInStock > 0).GroupBy(P => P.Category);
+
+            //Query 
+            //var Result = from P in ProductsList
+            //             where P.UnitsInStock > 0
+            //             group P by P.Category;
+
+            #endregion
+
+            #region Get Products in Stock Grouped by Category That Contains More Than 10 Product
+
+            // Fluent
+            //var Result = ProductsList.Where(P => P.UnitsInStock > 0).GroupBy(P => P.Category).Where(C => C.Count() > 10);
+
+            // Query
+            //var Result = from P in ProductsList
+            //             where P.UnitsInStock > 0
+            //             group P by P.Category
+            //             into category
+            //             where category.Count() > 10
+            //             select category;
+
+            #endregion
+
+            #region Get Category Name of Products in Stock That Contains More Than 10 Product and Number of Product In Each Category
+
+            //Fluent
+            //var Result = ProductsList.Where(P => P.UnitsInStock > 0).GroupBy( P => P.Category).Where(C => C.Count() > 10).Select(C => new {Name = C.Key, NumberOfProduct = C.Count()});
+
+            //Query
+            //var Result = from P in ProductsList
+            //             where P.UnitsInStock > 0
+            //             group P by P.Category
+            //             into Category
+            //             where Category.Count() > 10
+            //             select new
+            //             {
+            //                 Name = Category.Key,
+            //                 NumOfProduct = Category.Count(),
+            //             };
+            #endregion
+
+            #endregion
+
+            #region Partitioning Operators
+
+            //var Result = ProductsList.Take(10); // Take 10 Element from the First
+
+            //Result = ProductsList.Where( P => P.UnitsInStock > 0).TakeLast(10);
+
+            //Result = ProductsList.Skip(10); // Skip First 10 Element 
+
+            //Result = ProductsList.SkipLast(10); // Skip Last 10 Element 
+
+            //           //[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]    => I    
+            //int[] Nums = { 1, 6, 9, 3, 6, 2, 7, 4, 0, 8 };// => N
+
+            //var Result = Nums.TakeWhile((N, I) => N > I); // Take Element While Conditon Is true Till Element that do Not match condition stop.
+
+            //Result = Nums.SkipWhile((N, I) => N > I); // Skip Elements  while Condition is true Till Element That do not Match Condition
+            #endregion
+
+            #region Let and Into 
+
+            //List<string> Names02 = new List<string>() { "Omer", "Ahmed", "Adel", "Sara"};
+
+            //    //var Result = from N in Names02
+            //    //             select Regex.Replace(N, "[AEIOUaeiou]", string.Empty)
+            //    //             into NoVowelName
+            //    //             where NoVowelName.Length > 1
+            //    //             select NoVowelName;
+
+            //var Result = from N in Names02
+            //             let NoVowelName = Regex.Replace(N, "[AEIOUaeiou]", string.Empty)
+            //             where NoVowelName.Length > 1
+            //             select NoVowelName;
+
+            #endregion
+
+            //Console.WriteLine(Result);
+
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //foreach (var category in Result)
+            //{
+            //    Console.WriteLine(category.Key);
+            //    foreach (var P in category)
+            //    {
+            //        Console.WriteLine($"  {P.ProductName}");
+            //    }
+            //}
+        }
+    }
+}
